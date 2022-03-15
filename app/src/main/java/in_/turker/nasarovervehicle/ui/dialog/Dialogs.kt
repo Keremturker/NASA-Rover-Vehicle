b0f0561f0db74160e57d6238bc3a7849c75899eb
Kerem TÜRKER <alikeremturker@gmail.com>
2022-03-15 23:01:13 +0300

    ShowDetail dialog and ApplyFilter dialog view binding initialized. dialog_filter.xml UI edited.

--- app/src/main/java/in_/turker/nasarovervehicle/ui/dialog/Dialogs.kt
+++ app/src/main/java/in_/turker/nasarovervehicle/ui/dialog/Dialogs.kt
@@ -2,13 +2,13 @@ package in_.turker.nasarovervehicle.ui.dialog
 
 import android.app.Activity
 import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.Window
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RadioGroup
-import android.widget.TextView
 import in_.turker.nasarovervehicle.R
 import in_.turker.nasarovervehicle.data.model.Photo
+import in_.turker.nasarovervehicle.databinding.DialogDetailBinding
+import in_.turker.nasarovervehicle.databinding.DialogFilterBinding
 import in_.turker.nasarovervehicle.utils.CameraType
 import in_.turker.nasarovervehicle.utils.loadImagesWithGlide
 
@@ -23,70 +23,70 @@ fun applyFilter(
     Dialog(context).apply {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dialog_filter)
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val binding = DialogFilterBinding.inflate(inflater)
+        setContentView(binding.root)
+
         setCancelable(false)
 
         var cameraType: CameraType?
 
-        val btnApply = findViewById<Button>(R.id.btnApply)
-        val btnCancel = findViewById<Button>(R.id.btnCancel)
-        val rgCamera = findViewById<RadioGroup>(R.id.rgCamera)
+        binding.apply {
 
-        btnCancel.setOnClickListener {
-            if (!context.isFinishing) {
+            imgClose.setOnClickListener {
+                if (!context.isFinishing) {
+                    dismiss()
+                }
+            }
+
+            btnApply.setOnClickListener {
+
+                cameraType = when (rgCamera.checkedRadioButtonId) {
+
+                    R.id.rbFhaz -> {
+                        CameraType.FHAZ
+                    }
+                    R.id.rbRhaz -> {
+                        CameraType.RHAZ
+                    }
+                    R.id.rbMast -> {
+                        CameraType.MAST
+                    }
+                    R.id.rbChemcam -> {
+                        CameraType.CHEMCAM
+                    }
+                    R.id.rbMahli -> {
+                        CameraType.MAHLI
+                    }
+                    R.id.rbMardi -> {
+                        CameraType.MARDI
+                    }
+                    R.id.rbNavcam -> {
+                        CameraType.NAVCAM
+                    }
+                    R.id.rbPancam -> {
+                        CameraType.PANCAM
+                    }
+                    R.id.rbMinites -> {
+                        CameraType.MINITES
+                    }
+                    else -> {
+                        null
+                    }
+
+                }
+
+                okAction?.invoke(cameraType)
+
                 dismiss()
             }
+
         }
-
-        btnApply.setOnClickListener {
-
-            cameraType = when (rgCamera.checkedRadioButtonId) {
-
-                R.id.rbFhaz -> {
-                    CameraType.FHAZ
-                }
-                R.id.rbRhaz -> {
-                    CameraType.RHAZ
-                }
-                R.id.rbMast -> {
-                    CameraType.MAST
-                }
-                R.id.rbChemcam -> {
-                    CameraType.CHEMCAM
-                }
-                R.id.rbMahli -> {
-                    CameraType.MAHLI
-                }
-                R.id.rbMardi -> {
-                    CameraType.MARDI
-                }
-                R.id.rbNavcam -> {
-                    CameraType.NAVCAM
-                }
-                R.id.rbPancam -> {
-                    CameraType.PANCAM
-                }
-                R.id.rbMinites -> {
-                    CameraType.MINITES
-                }
-                else -> {
-                    null
-                }
-
-            }
-
-            okAction?.invoke(cameraType)
-
-            dismiss()
-        }
-
 
         if (!context.isFinishing) {
             show()
         }
     }
-
-
 }
 
 
@@ -97,29 +97,26 @@ fun showDetail(
     Dialog(context).apply {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dialog_detail)
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val binding = DialogDetailBinding.inflate(inflater)
+        setContentView(binding.root)
+
         setCancelable(false)
 
-        val imgPhoto = findViewById<ImageView>(R.id.imgPhoto)
-        val imgClose = findViewById<ImageView>(R.id.imgClose)
-        val txtTakePhotoDate = findViewById<TextView>(R.id.txtTakePhotoDate)
-        val txtVehicleName = findViewById<TextView>(R.id.txtVehicleName)
-        val txtCameraName = findViewById<TextView>(R.id.txtCameraName)
-        val txtVehicleStatus = findViewById<TextView>(R.id.txtVehicleStatus)
-        val txtLaunchDate = findViewById<TextView>(R.id.txtLaunchDate)
-        val txtLandingDate = findViewById<TextView>(R.id.txtLandingDate)
+        binding.apply {
+            imgPhoto.loadImagesWithGlide(vehicle.imgSrc)
+            txtTakePhotoDate.text = vehicle.earthDate
+            txtVehicleName.text = vehicle.rover.name
+            txtCameraName.text = vehicle.camera.name
+            txtVehicleStatus.text = vehicle.rover.status
+            txtLaunchDate.text = vehicle.rover.launchDate
+            txtLandingDate.text = vehicle.rover.landingDate
 
-        imgPhoto.loadImagesWithGlide(vehicle.imgSrc)
-        txtTakePhotoDate.text = vehicle.earthDate
-        txtVehicleName.text = vehicle.rover.name
-        txtCameraName.text = vehicle.camera.name
-        txtVehicleStatus.text = vehicle.rover.status
-        txtLaunchDate.text = vehicle.rover.launchDate
-        txtLandingDate.text = vehicle.rover.landingDate
-
-        imgClose.setOnClickListener {
-            if (!context.isFinishing) {
-                dismiss()
+            imgClose.setOnClickListener {
+                if (!context.isFinishing) {
+                    dismiss()
+                }
             }
         }
 
--- app/src/main/java/in_/turker/nasarovervehicle/utils/ViewExtensions.kt
+++ app/src/main/java/in_/turker/nasarovervehicle/utils/ViewExtensions.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import in_.turker.nasarovervehicle.R
 
 /**
  * Created by Kerem TÜRKER on 15.03.2022.
@@ -14,6 +15,7 @@ fun ImageView.loadImagesWithGlide(url: String?) {
         .load(url)
         .centerCrop()
         .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .placeholder(R.drawable.image_place_holder).error(R.drawable.image_error)
         .into(this)
 }
 
